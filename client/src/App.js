aafe3fdd3f47a2b8f1bdf9bb7264f12ec8b49b46
Jeffery <jefferywojo1998@gmail.com>
2022-01-15 22:42:00 -0600

    adding syntax to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -45,12 +45,12 @@ function App() {
             <Nav />
             <Routes>
               <Route exact path="/" element={<Home />} />
-              <Route exact path="/login" element={Login} />
-              <Route exact path="/signup" element={Signup} />
-              <Route exact path="/success" element={Success} />
-              <Route exact path="/orderHistory" element={OrderHistory} />
-              <Route exact path="/products/:id" element={Detail} />
-              <Route element={NoMatch} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/signup" element={<Signup />} />
+              <Route exact path="/success" element={<Success />} />
+              <Route exact path="/orderHistory" element={<OrderHistory />} />
+              <Route exact path="/products/:id" element={<Detail />} />
+              <Route element={< NoMatch />} />
             </Routes>
           </Provider>
         </div>
