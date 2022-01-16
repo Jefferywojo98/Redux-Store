6bfd21d8f338935138f489b5cea55380a7faa3ca
Jeffery <jefferywojo1998@gmail.com>
2022-01-16 01:27:20 -0600

    getting rid of GlobalState

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,15 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
+import { Provider } from 'react-redux';
+import store from './utils/store';
+
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
 
@@ -42,18 +44,18 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider >
+          <Provider store={store}>
             <Nav />
             <Switch>
-            <Route exact path="/" element={<Home />} />
-              <Route exact path="/login" element={<Login />} />
-              <Route exact path="/signup" element={<Signup />} />
-              <Route exact path="/success" element={<Success />} />
-              <Route exact path="/orderHistory" element={<OrderHistory />} />
-              <Route exact path="/products/:id" element={<Detail />} />
-              <Route element={< NoMatch />} />
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/success" component={Success} />
+              <Route exact path="/orderHistory" component={OrderHistory} />
+              <Route exact path="/products/:id" component={Detail} />
+              <Route component={NoMatch} />
             </Switch>
-          </StoreProvider>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
