6b299bf15f2a2ec89f8d4b7eb39d88e3e3f7d0f6
Jeffery <jefferywojo1998@gmail.com>
2022-01-15 19:59:18 -0600

    no more error on terminal

--- client/src/App.js
+++ client/src/App.js
@@ -44,15 +44,15 @@ function App() {
         <div>
           <StoreProvider>
             <Nav />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={Signup} />
-              <Route exact path="/success" component={Success} />
-              <Route exact path="/orderHistory" component={OrderHistory} />
-              <Route exact path="/products/:id" component={Detail} />
-              <Route component={NoMatch} />
-            </Switch>
+            <Route />
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/login" element={Login} />
+              <Route exact path="/signup" element={Signup} />
+              <Route exact path="/success" element={Success} />
+              <Route exact path="/orderHistory" element={OrderHistory} />
+              <Route exact path="/products/:id" element={Detail} />
+              <Route element={NoMatch} />
+              <Route />
           </StoreProvider>
         </div>
       </Router>
