f10bfc64564cddb44b88a287cc0bfe89f15bb790
Jeffery <jefferywojo1998@gmail.com>
2022-01-16 01:29:22 -0600

    for rid of some route location by accident

--- client/src/App.js
+++ client/src/App.js
@@ -39,21 +39,22 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+
 function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <Provider store={store}>
+        <Provider store={store}>
             <Nav />
             <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={Signup} />
-              <Route exact path="/success" component={Success} />
-              <Route exact path="/orderHistory" component={OrderHistory} />
-              <Route exact path="/products/:id" component={Detail} />
-              <Route component={NoMatch} />
+            <Route exact path="/" element={<Home />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/signup" element={<Signup />} />
+              <Route exact path="/success" element={<Success />} />
+              <Route exact path="/orderHistory" element={<OrderHistory />} />
+              <Route exact path="/products/:id" element={<Detail />} />
+              <Route element={< NoMatch />} />
             </Switch>
           </Provider>
         </div>
